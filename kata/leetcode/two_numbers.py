0df4ce11dbf754c5d79e9ef341f11ffcfefd3b2a
Xiaoming Gao <xiaoming.gao@rea-group.com>
2022-05-24 23:39:45 +0800

    two numbers with negative num O(n)

--- kata/leetcode/two_numbers.py
+++ kata/leetcode/two_numbers.py
@@ -2,16 +2,29 @@ from typing import List
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
 
+    # violence O(n^2)
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #
+    #     result = []
+    #
+    #     for pos_l, value_l in enumerate(nums):
+    #         for pos_r, value_r in enumerate(nums):
+    #             if value_l + value_r == target and pos_l != pos_r:
+    #                 result.append(pos_l)
+    #                 result.append(pos_r)
+    #                 return result
+
+    # O(n)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         result = []
 
-        for pos_l, value_l in enumerate(nums):
-            for pos_r, value_r in enumerate(nums):
-                if value_l + value_r == target and pos_l != pos_r:
-                    result.append(pos_l)
-                    result.append(pos_r)
-                    return result
+        for pos, value in enumerate(nums):
+            p = list.copy(nums)
+            p.remove(value)
+            if target - value in p:
+                result.append(pos)
+        return result
 
 
 if __name__ == "__main__":
