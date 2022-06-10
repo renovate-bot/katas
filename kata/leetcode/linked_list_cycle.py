37df304bcb1ca36312ae9466383938ce415f68b2
Xiaoming Gao <xiaoming.gao@rea-group.com>
2022-06-10 23:56:53 +0800

    Slow pointer and fast pointer

--- kata/leetcode/linked_list_cycle.py
+++ kata/leetcode/linked_list_cycle.py
@@ -1,3 +1,7 @@
+"""
+https://leetcode.cn/problems/linked-list-cycle
+"""
+
 from typing import Optional
 
 
@@ -8,6 +12,20 @@ class ListNode:
 
 
 class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        start = ListNode(0)
+        start.next = head
+        slow = start
+        fast = start.next
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
+
+
+class FirstSolution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         linked_list_hash = {}
         p = head
