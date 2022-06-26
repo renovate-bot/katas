1cdd9d1db5db35ea717bba8e4b4e7eb8793c3aa7
Xiaoming Gao <xiaoming.gao@rea-group.com>
2022-06-26 22:01:31 +0800

    iteration

--- kata/leetcode/binary_tree_preorder_traversal.py
+++ kata/leetcode/binary_tree_preorder_traversal.py
@@ -4,8 +4,25 @@ from kata.leetcode.common import TreeNode
 
 
 class Solution:
+    # recursion
+    # def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    #     if not root:
+    #         return []
+    #     return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
+    # iteration
 
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+        result, temp = [], [root]
+
+        while temp:
+            node = temp.pop()
+            result.append(node.val)
+
+            if node.left:
+                temp.append(node.left)
+            if node.right:
+                temp.append(node.right)
+        return result
