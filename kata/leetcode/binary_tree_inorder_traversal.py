e6ce68752c5ec056ffa42a3079e52e11b56b7972
Xiaoming Gao <xiaoming.gao@rea-group.com>
2022-07-03 22:38:36 +0800

    in order

--- kata/leetcode/binary_tree_inorder_traversal.py
+++ kata/leetcode/binary_tree_inorder_traversal.py
@@ -5,7 +5,26 @@ from kata.leetcode.common import TreeNode
 
 class Solution:
 
+    # def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    #     if not root:
+    #         return []
+    #     return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        result, node, stack = [], root, []
+
         if not root:
             return []
-        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+
+        while node or stack:
+
+            while node:
+                stack.append(node)
+                node = node.left
+
+            node = stack.pop()
+            result.append(node.val)
+            node = node.right
+
+        return result
--- kata/leetcode/binary_tree_postorder_traversal.py
+++ kata/leetcode/binary_tree_postorder_traversal.py
@@ -5,9 +5,25 @@ from kata.leetcode.common import TreeNode
 
 class Solution:
     # recursion
+    # def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    #     if not root:
+    #         return []
+    #     return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+
+    # iteration
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-        return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+        result, temp = [], [root]
 
-    # iteration
+        while temp:
+            node = temp[0]
+            del temp[0]
+            result.insert(0, node.val)
+
+            if node.left:
+                temp.append(node.left)
+            if node.right:
+                temp.append(node.right)
+
+        return result
