0c3e7df0cb87866c94b61bafff2d5f7c16a20f2b
Xiaoming Gao <xiaoming.gao@rea-group.com>
2022-07-19 22:41:17 +0800

    symmetric tree

--- kata/leetcode/symmetric_tree.py
+++ kata/leetcode/symmetric_tree.py
@@ -5,49 +5,61 @@ from kata.leetcode.common import TreeNode
 
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        if self.max_depth(root.left) != self.max_depth(root.right):
-            return False
-        left_tree = self.breadth_travel_left(root.left)
-        reversed_right_tree = self.breadth_travel_right(root.right)
 
-        print(f"left tree: {left_tree}")
-        print(f"right tree: {reversed_right_tree}")
-        if left_tree == reversed_right_tree:
+        return self.bfs(root.left, root.right)
+
+    def bfs(self, left: TreeNode, right: TreeNode):
+        if not left and not right:
             return True
-        return False
-
-    @staticmethod
-    def max_depth(root: Optional[TreeNode]):
-        if not root:
-            return 0
-        return max(Solution.max_depth(root.left), Solution.max_depth(root.right)) + 1
-
-    @staticmethod
-    def breadth_travel_left(root: Optional[TreeNode]):
-        if root is None:
-            return []
-        queue = [root]
-        result = []
-        while queue:
-            node = queue.pop(0)
-            result.append(node.val)
-            if node.left is not None:
-                queue.append(node.left)
-            if node.right is not None:
-                queue.append(node.right)
-        return result
-
-    @staticmethod
-    def breadth_travel_right(root: Optional[TreeNode]):
-        if root is None:
-            return []
-        queue = [root]
-        result = []
-        while queue:
-            node = queue.pop(0)
-            result.append(node.val)
-            if node.right is not None:
-                queue.append(node.right)
-            if node.left is not None:
-                queue.append(node.left)
-        return result
+        if not left or not right:
+            return False
+        if left.val != right.val:
+            return False
+        return self.bfs(left.left, right.right) and self.bfs(left.right, right.left)
+    # def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+    #     if self.max_depth(root.left) != self.max_depth(root.right):
+    #         return False
+    #     left_tree = self.breadth_travel_left(root.left)
+    #     reversed_right_tree = self.breadth_travel_right(root.right)
+    #
+    #     print(f"left tree: {left_tree}")
+    #     print(f"right tree: {reversed_right_tree}")
+    #     if left_tree == reversed_right_tree:
+    #         return True
+    #     return False
+    #
+    # @staticmethod
+    # def max_depth(root: Optional[TreeNode]):
+    #     if not root:
+    #         return 0
+    #     return max(Solution.max_depth(root.left), Solution.max_depth(root.right)) + 1
+    #
+    # @staticmethod
+    # def breadth_travel_left(root: Optional[TreeNode]):
+    #     if root is None:
+    #         return []
+    #     queue = [root]
+    #     result = []
+    #     while queue:
+    #         node = queue.pop(0)
+    #         result.append(node.val)
+    #         if node.left is not None:
+    #             queue.append(node.left)
+    #         if node.right is not None:
+    #             queue.append(node.right)
+    #     return result
+    #
+    # @staticmethod
+    # def breadth_travel_right(root: Optional[TreeNode]):
+    #     if root is None:
+    #         return []
+    #     queue = [root]
+    #     result = []
+    #     while queue:
+    #         node = queue.pop(0)
+    #         result.append(node.val)
+    #         if node.right is not None:
+    #             queue.append(node.right)
+    #         if node.left is not None:
+    #             queue.append(node.left)
+    #     return result
